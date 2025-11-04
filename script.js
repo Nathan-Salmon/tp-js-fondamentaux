3da991406c4bd21902d5df8e07de0a1e2444b42d
Nathan-Salmon <nsalmon369@gmail.com>
2025-11-04 08:18:23 +0000

    feat: Maîtrise des Objets JS

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-console.log("laboratoir prêt !")
-const nom = "Nathan";
-let age = 18;
-//nom = "bonnamour"
-//Uncaught SyntaxError: Numeric separators are not allowed at the end of numeric literals
-console.log(age)
-
-const motDePasseAttendu = "secret123";
-let motDePasseUtilisateur = "bonjour123"; // Changez cette valeur pour tester
-
-if ( motDePasseAttendu === motDePasseUtilisateur ) {
-    console.log("Accès autorisé. Bienvenue !");
-} else if ( motDePasseUtilisateur.length< 9) {
-    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
-} else {
-    console.log("Accès refusé. Mot de passe incorrect.");
+const moi = {
+    prenom: "Nathan",
+    nom: "Salmon",
+    age: 18 ,
+    ville: "Lyon",
+    competences: ["HTML", "CSS"]
 }
+console.log("Je m'appelle" ,moi.prenom, moi.nom, moi.age, moi.ville);
+console.log(moi)
